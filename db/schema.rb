d23326d9ff665c8e2fde112a956d2b7c46fee4eb
jacubco <jacubco@tutanota.com>
2018-07-18 10:29:28 +0200

    added prices to data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_073800) do
+ActiveRecord::Schema.define(version: 2018_07_18_075726) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_18_073800) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "colour"
+    t.string "color"
     t.float "price"
   end
 
