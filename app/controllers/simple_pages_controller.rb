1c4235d6d3e362cc5f5e94d24699285e69523d2d
jacubco <jacubco@tutanota.com>
2018-07-19 22:26:28 +0200

    added code to dynamically change carousel indicators on landing_page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,7 +2,7 @@ class SimplePagesController < ApplicationController
   def index
   end
   def landing_page
-  	@products = Product.limit(3)
+  	@products = Product.limit(4)
   	@featured_product = Product.first
   end
 end
