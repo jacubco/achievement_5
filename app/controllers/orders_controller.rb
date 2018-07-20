bb2a81d9291b04845be6a64d52e2756e6d906c90
jacubco <jacubco@tutanota.com>
2018-07-20 10:21:33 +0200

    changed index method in orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
   def index
   	# Querys order table, and then retrieves each product from product table
-  	# @orders = Order.all
+  	@orders = Order.all
   	
 
   	# Cleaner and better version
   	# Includes the products in the query for orders
   	# Less queries overall 
-  	@orders = Order.includes(:product).all
+  	# @orders = Order.includes(:product).all
   	
   end
 
