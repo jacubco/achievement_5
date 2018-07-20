6088b89a213112fb4858e48d0d17cf2e58a01c0f
jacubco <jacubco@tutanota.com>
2018-07-20 09:54:05 +0200

    optimized orders query for display in orders/index.html.erb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,14 @@
 class OrdersController < ApplicationController
   def index
-  	@orders = Order.all
+  	# Querys order table, and then retrieves each product from product table
+  	# @orders = Order.all
+  	
+
+  	# Cleaner and better version
+  	# Includes the products in the query for orders
+  	# Less queries overall 
+  	@orders = Order.includes(:product).all
+  	
   end
 
   def show
