a14c066a682ecfa7b54e5f53cdaae1f45783f137
jacubco <jacubco@tutanota.com>
2018-07-22 16:13:27 +0200

    deleted postgress ilike from product controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,6 @@ class Product < ApplicationRecord
   # SQL LIKE operator used to find product if name does not completly match search term
   # ilike is the equivalent operator in POSTGRES (production environment) 
   def self.search(search_term)
-    if Rails.env.development?
       Product.where("name LIKE ?", "%#{search_term}%")
-    else
-      Product.where("name ilike ?", "%#{search_term}%")
-    end
   end
 end
