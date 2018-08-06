fe7dfe8c4fa823c2a33882059a94b14734b8156a
jacubco <jacubco@tutanota.com>
2018-08-06 13:29:15 +0200

    add validation logic to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,16 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to product_path(@product)
-  end
+    
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
 
   def destroy
   end
@@ -15,6 +22,9 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
+
+  
+
 
 end
 
