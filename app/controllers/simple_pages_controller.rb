fdc38be7db6875413a97b89681e7c1dc0c70a50b
jacubco <jacubco@tutanota.com>
2018-08-19 16:49:26 +0200

    define rule for featured products in simple_pages controller

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,8 +3,9 @@ class SimplePagesController < ApplicationController
   end
   def landing_page
     @products = Product.limit(2)
-    @featured_products = Product.limit(3)
+    @featured_products = Product.where(featured: true)
   end
+
 
   def about
     # redirect_to '/simple_pages/contact'
@@ -15,6 +16,5 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     UserMailer.contact_form(@email, @name, @message).deliver_now
-
   end
 end
