92391a51b301fbb783e1730fc1718e76ccdd4af1
jacubco <jacubco@tutanota.com>
2018-09-12 15:34:08 +0200

    Display new comments immediately using ActionCable

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,31 +1,32 @@
+$(document).on('turbolinks:load', function() {
+
 App.product = App.cable.subscriptions.create("ProductChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-  },
+connected: function() {
+App.product.listenToComments()
+// Called when the subscription is ready for use on the server
+},
 
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
+disconnected: function() {
+// Called when the subscription has been terminated by the server
+},
 
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
+received: function(data) {
+// Called when there's incoming data on the websocket for this channel
 
-    
-    $(".alert.alert-info").show();
+$(".alert.alert-info").show();
 
-  },
+  $('.product-reviews').prepend(data.comment);
+  $("#average-rating").attr('data-score', data.average_rating);
+  refreshRating();
 
-  listen_to_comments: function() {
-    // App refers to everything that is defined above
-    // App.product is the subscriber object for the product channel
-    // You could also use this.perform() instead
-    return App.product.perform('listen', {
-      product_id: $("[data-product-id]").data("product-id")
-    });
-  }
+console.log(data);
+},
 
+listenToComments: function() {
+return this.perform('listen', {
+product_id: $("[data-product-id]").data("product-id")
+});
+}
 });
 
-$(document).on('turbolinks:load', function() {
-  App.product.listen_to_comments();
 });
